d7e4b40a99709668c8d87449ea53f20b6a4aad82
Admin <makwanadharmishtha44@gmail.com>
2022-12-12 06:10:01 +0530

    red and white

---
+++ android/app/src/main/kotlin/com/example/redandwhite1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.redandwhite1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red,
+              centerTitle: true,
+              title: Text("My RNW"),
+            ),
+            body: Align(
+              alignment: Alignment.center,
+              child: RichText(
+                text: TextSpan(
+                  children:[
+                    TextSpan(
+                      text: "Red & White\n",
+                      style: TextStyle(fontSize: 75,color: Colors.red,decoration: TextDecoration.underline,fontWeight: FontWeight.bold)
+                    ),
+                    TextSpan(
+                      children: [
+                        // Align(
+                        //   alignment: Alignment.center,
+                        // ),
+                        TextSpan(
+                          text: "             Multimedeia Education\n",
+                          style: TextStyle(fontSize: 25,color: Colors.red,fontWeight: FontWeight.bold)
+                        )
+                      ],
+                    ),
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "            Shaping ''Skill''for''scaling''higher...!\n",
+                          style: TextStyle(fontSize: 20,color: Colors.red)
+                        ),
+                      ]
+                    ),
+                  ]
+                ),
+              ),
+            )
+          ),
+        ),
+        ) ,
+      );
+}
